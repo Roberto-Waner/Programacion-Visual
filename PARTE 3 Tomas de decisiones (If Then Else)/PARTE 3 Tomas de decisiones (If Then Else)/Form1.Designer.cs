744d2bc409d90778db325917def02b6433b1fa5e
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2024-01-24 19:51:42 -0400

    Practica

--- PARTE 3 Tomas de decisiones (If Then Else)/PARTE 3 Tomas de decisiones (If Then Else)/Form1.Designer.cs
+++ PARTE 3 Tomas de decisiones (If Then Else)/PARTE 3 Tomas de decisiones (If Then Else)/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txt_Empresa = new System.Windows.Forms.TextBox();
-            this.txt_Monto = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txt_Tiempo = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,65 +40,66 @@
             this.txt_TasaInterEX = new System.Windows.Forms.TextBox();
             this.lst_Resul = new System.Windows.Forms.ListBox();
             this.btn_Analisis = new System.Windows.Forms.Button();
-            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_general = new System.Windows.Forms.Button();
             this.btn_Fin = new System.Windows.Forms.Button();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.txt_Monto = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre de Empresa:";
             // 
             // txt_Empresa
             // 
-            this.txt_Empresa.Location = new System.Drawing.Point(164, 20);
+            this.txt_Empresa.Location = new System.Drawing.Point(123, 16);
+            this.txt_Empresa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Empresa.Name = "txt_Empresa";
-            this.txt_Empresa.Size = new System.Drawing.Size(402, 22);
+            this.txt_Empresa.Size = new System.Drawing.Size(302, 20);
             this.txt_Empresa.TabIndex = 1;
-            // 
-            // txt_Monto
-            // 
-            this.txt_Monto.Location = new System.Drawing.Point(92, 66);
-            this.txt_Monto.Name = "txt_Monto";
-            this.txt_Monto.Size = new System.Drawing.Size(124, 22);
-            this.txt_Monto.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Location = new System.Drawing.Point(16, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Monto prestamo ($)";
             // 
             // txt_Tiempo
             // 
-            this.txt_Tiempo.Location = new System.Drawing.Point(449, 69);
+            this.txt_Tiempo.Location = new System.Drawing.Point(337, 53);
+            this.txt_Tiempo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Tiempo.Name = "txt_Tiempo";
-            this.txt_Tiempo.Size = new System.Drawing.Size(117, 22);
+            this.txt_Tiempo.Size = new System.Drawing.Size(89, 20);
             this.txt_Tiempo.TabIndex = 5;
             this.txt_Tiempo.Text = "2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(343, 72);
+            this.label3.Location = new System.Drawing.Point(257, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Tiempo (años)";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 147);
+            this.label4.Location = new System.Drawing.Point(16, 119);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 16);
+            this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Tasa Interes:";
             // 
@@ -107,42 +107,49 @@
             // 
             this.rdb_Interes1.AutoSize = true;
             this.rdb_Interes1.Checked = true;
-            this.rdb_Interes1.Location = new System.Drawing.Point(25, 211);
+            this.rdb_Interes1.Location = new System.Drawing.Point(19, 171);
+            this.rdb_Interes1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rdb_Interes1.Name = "rdb_Interes1";
-            this.rdb_Interes1.Size = new System.Drawing.Size(57, 20);
+            this.rdb_Interes1.Size = new System.Drawing.Size(48, 17);
             this.rdb_Interes1.TabIndex = 7;
             this.rdb_Interes1.TabStop = true;
             this.rdb_Interes1.Text = "12 %";
             this.rdb_Interes1.UseVisualStyleBackColor = true;
+            this.rdb_Interes1.Click += new System.EventHandler(this.rdb_Interes1_Click);
             // 
             // rdb_Interes2
             // 
             this.rdb_Interes2.AutoSize = true;
-            this.rdb_Interes2.Location = new System.Drawing.Point(156, 211);
+            this.rdb_Interes2.Location = new System.Drawing.Point(117, 171);
+            this.rdb_Interes2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rdb_Interes2.Name = "rdb_Interes2";
-            this.rdb_Interes2.Size = new System.Drawing.Size(67, 20);
+            this.rdb_Interes2.Size = new System.Drawing.Size(57, 17);
             this.rdb_Interes2.TabIndex = 8;
             this.rdb_Interes2.TabStop = true;
             this.rdb_Interes2.Text = "23.5 %";
             this.rdb_Interes2.UseVisualStyleBackColor = true;
+            this.rdb_Interes2.Click += new System.EventHandler(this.rdb_Interes2_Click);
             // 
             // rdb_Interes3
             // 
             this.rdb_Interes3.AutoSize = true;
-            this.rdb_Interes3.Location = new System.Drawing.Point(294, 211);
+            this.rdb_Interes3.Location = new System.Drawing.Point(220, 171);
+            this.rdb_Interes3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rdb_Interes3.Name = "rdb_Interes3";
-            this.rdb_Interes3.Size = new System.Drawing.Size(106, 20);
+            this.rdb_Interes3.Size = new System.Drawing.Size(88, 17);
             this.rdb_Interes3.TabIndex = 9;
             this.rdb_Interes3.TabStop = true;
             this.rdb_Interes3.Text = "Otro, indique:";
             this.rdb_Interes3.UseVisualStyleBackColor = true;
+            this.rdb_Interes3.CheckedChanged += new System.EventHandler(this.rdb_Interes3_CheckedChanged);
             // 
             // txt_TasaInterEX
             // 
             this.txt_TasaInterEX.Enabled = false;
-            this.txt_TasaInterEX.Location = new System.Drawing.Point(413, 209);
+            this.txt_TasaInterEX.Location = new System.Drawing.Point(310, 170);
+            this.txt_TasaInterEX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_TasaInterEX.Name = "txt_TasaInterEX";
-            this.txt_TasaInterEX.Size = new System.Drawing.Size(153, 22);
+            this.txt_TasaInterEX.Size = new System.Drawing.Size(116, 20);
             this.txt_TasaInterEX.TabIndex = 10;
             this.txt_TasaInterEX.Text = "0";
             // 
@@ -150,46 +157,70 @@
             // 
             this.lst_Resul.Enabled = false;
             this.lst_Resul.FormattingEnabled = true;
-            this.lst_Resul.ItemHeight = 16;
-            this.lst_Resul.Location = new System.Drawing.Point(25, 265);
+            this.lst_Resul.Location = new System.Drawing.Point(19, 215);
+            this.lst_Resul.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lst_Resul.Name = "lst_Resul";
-            this.lst_Resul.Size = new System.Drawing.Size(550, 132);
+            this.lst_Resul.Size = new System.Drawing.Size(414, 108);
             this.lst_Resul.TabIndex = 11;
             // 
             // btn_Analisis
             // 
-            this.btn_Analisis.Location = new System.Drawing.Point(90, 442);
+            this.btn_Analisis.Location = new System.Drawing.Point(20, 347);
+            this.btn_Analisis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Analisis.Name = "btn_Analisis";
-            this.btn_Analisis.Size = new System.Drawing.Size(110, 50);
+            this.btn_Analisis.Size = new System.Drawing.Size(82, 41);
             this.btn_Analisis.TabIndex = 12;
             this.btn_Analisis.Text = "Análisis \r\nFinanciero";
             this.btn_Analisis.UseVisualStyleBackColor = true;
+            this.btn_Analisis.Click += new System.EventHandler(this.btn_Analisis_Click);
             // 
-            // btn_guardar
+            // btn_general
             // 
-            this.btn_guardar.Location = new System.Drawing.Point(238, 442);
-            this.btn_guardar.Name = "btn_guardar";
-            this.btn_guardar.Size = new System.Drawing.Size(110, 50);
-            this.btn_guardar.TabIndex = 13;
-            this.btn_guardar.Text = "Guardar";
-            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_general.Location = new System.Drawing.Point(130, 347);
+            this.btn_general.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_general.Name = "btn_general";
+            this.btn_general.Size = new System.Drawing.Size(82, 41);
+            this.btn_general.TabIndex = 13;
+            this.btn_general.Text = "Generar Amortizacion";
+            this.btn_general.UseVisualStyleBackColor = true;
             // 
             // btn_Fin
             // 
-            this.btn_Fin.Location = new System.Drawing.Point(388, 442);
+            this.btn_Fin.Location = new System.Drawing.Point(351, 347);
+            this.btn_Fin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Fin.Name = "btn_Fin";
-            this.btn_Fin.Size = new System.Drawing.Size(110, 50);
+            this.btn_Fin.Size = new System.Drawing.Size(82, 41);
             this.btn_Fin.TabIndex = 14;
-            this.btn_Fin.Text = "Salir del \r\nprograma";
+            this.btn_Fin.Text = "Terminar Programa";
             this.btn_Fin.UseVisualStyleBackColor = true;
+            this.btn_Fin.Click += new System.EventHandler(this.btn_Fin_Click);
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(240, 347);
+            this.btn_mostrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(91, 41);
+            this.btn_mostrar.TabIndex = 15;
+            this.btn_mostrar.Text = "Mostrar Tabla \r\nAmortizada";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            // 
+            // txt_Monto
+            // 
+            this.txt_Monto.Location = new System.Drawing.Point(118, 53);
+            this.txt_Monto.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Monto.Name = "txt_Monto";
+            this.txt_Monto.Size = new System.Drawing.Size(94, 20);
+            this.txt_Monto.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 521);
+            this.ClientSize = new System.Drawing.Size(453, 423);
+            this.Controls.Add(this.btn_mostrar);
             this.Controls.Add(this.btn_Fin);
-            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_general);
             this.Controls.Add(this.btn_Analisis);
             this.Controls.Add(this.lst_Resul);
             this.Controls.Add(this.txt_TasaInterEX);
@@ -203,9 +234,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txt_Empresa);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,7 +246,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_Empresa;
-        private System.Windows.Forms.TextBox txt_Monto;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_Tiempo;
         private System.Windows.Forms.Label label3;
@@ -226,8 +256,10 @@
         private System.Windows.Forms.TextBox txt_TasaInterEX;
         private System.Windows.Forms.ListBox lst_Resul;
         private System.Windows.Forms.Button btn_Analisis;
-        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_general;
         private System.Windows.Forms.Button btn_Fin;
+        private System.Windows.Forms.Button btn_mostrar;
+        private System.Windows.Forms.TextBox txt_Monto;
     }
 }
 
--- PARTE 3 Tomas de decisiones (If Then Else)/PARTE 3 Tomas de decisiones (If Then Else)/Form1.cs
+++ PARTE 3 Tomas de decisiones (If Then Else)/PARTE 3 Tomas de decisiones (If Then Else)/Form1.cs
@@ -17,9 +17,105 @@ namespace PARTE_3_Tomas_de_decisiones__If_Then_Else_
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private double tasal;
 
+        public static Boolean IsNumeric(string valor)
+        {
+            int result;
+            return int.TryParse(valor, out result);
+        }
+
+        private void rdb_Interes3_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdb_Interes3.Checked == true)
+            {
+                txt_TasaInterEX.Enabled = true;
+                txt_TasaInterEX.Focus();
+            }
+            else
+            {
+                txt_TasaInterEX.Text = "0";
+                txt_TasaInterEX.Enabled = false;
+            }
+        }
+
+        private void rdb_Interes1_Click(object sender, EventArgs e)
+        {
+            tasal = 0.12;
+        }
+
+        private void rdb_Interes2_Click(object sender, EventArgs e)
+        {
+            tasal = 0.235;
+        }
+
+        private void btn_Fin_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Analisis_Click(object sender, EventArgs e)
+        {
+            string Nom_Empresa;
+            double MontoInic = 0, MontoFin = 0;
+            int Tiempo;
+
+            Nom_Empresa = txt_Empresa.Text;
+            Nom_Empresa = Nom_Empresa.Trim();
+
+            if(Nom_Empresa.Length == 0)
+            {
+                MessageBox.Show("Bebe indicar Nombre de la empresa", "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txt_Monto.Focus();
+                return;
+            }
+
+            if (!(IsNumeric(txt_Monto.Text)))
+            {
+                MessageBox.Show("Valor Monto incorrecto", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_Monto.Focus();
+                return;
+            }
+            else
+            {
+                MontoInic = Convert.ToDouble(txt_Monto.Text);
+
+                if(!(MontoInic > 0))
+                {
+                    MessageBox.Show("Valor Monto no puede ser negativo", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_Monto.Focus();
+                    return;
+                }
+            }
+
+            Tiempo = Convert.ToInt32(txt_Tiempo.Text);
+            txt_TasaInterEX.Text = txt_TasaInterEX.Text.Trim();
+
+            if(rdb_Interes3.Checked == true)
+            {
+                if(txt_TasaInterEX.Text.Length > 0)
+                {
+                    if(!(IsNumeric(txt_TasaInterEX.Text) == true))
+                    {
+                        MessageBox.Show("Tasa interes incorrecto", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txt_TasaInterEX.Text = "0";
+                        txt_TasaInterEX.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        tasal = Convert.ToDouble(txt_TasaInterEX.Text) / 100;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Aun no ha indicado una tasa interes", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_TasaInterEX.Focus();
+                return;
+            }
         }
     }
 }
